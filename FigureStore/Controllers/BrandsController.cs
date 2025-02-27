030ff7175841b8cb163804ee646cd247bdf273c2
Duy Tai <0985203883tai@gmail.com>
2025-02-27 18:56:13 +0700

    crud Brands

--- FigureStore/Controllers/BrandsController.cs
+++ FigureStore/Controllers/BrandsController.cs
@@ -36,6 +36,67 @@ namespace FigureStore.Controllers
         }
 
         // Nếu cần: POST, PUT, DELETE cho các thao tác khác
+        // POST: api/Brands
+        [HttpPost]
+        public async Task<ActionResult<Brand>> CreateBrand(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            await _context.SaveChangesAsync();
 
+            // CreatedAtAction trả về mã 201 kèm theo header Location trỏ đến API GetBrand
+            return CreatedAtAction(nameof(GetBrand), new { id = brand.Id }, brand);
+        }
+
+        // PUT: api/Brands/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBrand(int id, Brand brand)
+        {
+            // 1. Kiểm tra id param và brand.Id trong body
+            if (id != brand.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(brand).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BrandExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Brands/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            _context.Brands.Remove(brand);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BrandExists(int id)
+        {
+            return _context.Brands.Any(e => e.Id == id);
+        }
     }
 }
